6635fbe3e32cbfe91bec546c60fa202871b0093d
admin <2608656305@qq.com>
2020-04-06 00:27:08 +0800

    加入jdbc_demo

--- src/JDBC/demo/Demo1.java
+++ src/JDBC/demo/Demo1.java
@@ -1,4 +1,23 @@
 package JDBC.demo;
 
+import java.sql.*;
+
 public class Demo1 {
+    public static void  main(String[] args) throws ClassNotFoundException, SQLException {
+         final String DB_url="jdbc:mysql://localhost:3306/mydemo?useSSL=false&serverTimezone=UTC";
+
+           Class.forName("com.mysql.cj.jdbc.Driver");
+           System.out.println("链接数据库");
+        Connection connection = DriverManager.getConnection(DB_url,"root","123456");
+        System.out.println("实列化身体Statement");
+        Statement st = connection.createStatement();
+      ResultSet rs=  st.executeQuery("SELECT * FROM demo");
+        while (rs.next()) {
+            System.out.println(rs.getInt(1) + rs.getString(2));
+        }
+        rs.close();
+        st.close();
+        connection.close();
+
+    }
 }
